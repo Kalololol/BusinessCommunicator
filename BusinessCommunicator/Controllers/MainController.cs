238c40a0dbd618dcc75133a3d8bd8459310752c0
Karol kalololol <karol.binkowski92@gmail.com>
2022-06-11 11:57:06 +0200

    Write code

--- BusinessCommunicator/Controllers/MainController.cs
+++ BusinessCommunicator/Controllers/MainController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessCommunicator.Models;
+using BusinessCommunicator.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace BusinessCommunicator.Controllers
 {
     public class MainController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
